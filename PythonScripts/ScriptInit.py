eee7e53320e14987b194de94c5f2504337b3006a
robgoyal <robin_goyal@hotmail.com>
2016-07-21 12:13:22 -0400

    Support for Bash, C++, HTML, CSS

--- PythonScripts/ScriptInit.py
+++ PythonScripts/ScriptInit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/python
+#!/usr/local/bin/python
 
 # ScriptInit.py
 # Code written by Robin Goyal
@@ -9,40 +9,78 @@
 
 # Script accepts two arguments:
 # - File name to be created
-# - Name of the user
+# - Name of the user accepted as a string in quotation marks
 
 from sys import argv
 import subprocess
 import datetime
 
+# Try/Except for argument inputs
 try:
     script, filename, user_name = argv
     current_time = datetime.datetime.now()
 
     extension = filename.split(".")[1:]
+    subprocess.call(["touch", filename])
+    file = open(filename, 'w')
 
     if extension[0] == "py":
-        subprocess.call(["touch", filename])
-
-        file = open(filename, 'w')
         file.write("#!/usr/bin/env/python\n\n")
         file.write("# Code written by " + user_name + "\n")
         file.write("# Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n")
         file.write("# Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n\n")
-        description = raw_input("Would you like to provide a description of your project? Press RETURN if no description is needed. \n> ")
-        file.write("# " + description)
-        file.close()
+        description = raw_input("Would you like to provide a description of your project? Enter No if a description is not needed. \n> ")
+
+        if not(description == "No" or description == "NO" or description == "no"):
+            file.write("# " + description)
 
     elif extension[0] == "c":
-        subprocess.call(["touch", filename])
-
         file = open(filename, 'w')
-        file.write("/* Code written by " + user_name + "*/\n")
-        file.write("/* Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "*/\n")
-        file.write("/* Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "*/\n\n")
-        description = raw_input("Would you like to provide a description of our project? Press RETURN if no description is needed. \n> ")
-        file.write("/* " + description + " */")
+        file.write("/* Code written by " + user_name + " */\n")
+        file.write("/* Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + " */\n")
+        file.write("/* Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + " */\n\n")
+        description = raw_input("Would you like to provide a description of our project? Enter No if a description is not needed. \n> ")
+        if not(description == "No" or description == "NO" or description == "no"):
+            file.write("/* " + description + " */")
+
+    elif extension[0] == "cpp" or extension[0] == "c++" or extension[0] == "cp":
+        file.write("// Code written by " + user_name + "\n")
+        file.write("// Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n")
+        file.write("// Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n\n")
+        description = raw_input("Would you like to provide a description of your project? Enter No if a description is not needed. \n> ")
+        if not(description == "No" or description == "NO" or description == "no"):
+            file.write("// " + description)
+
+    elif extension[0] == "sh":
+        file.write("#!/bin/bash\n\n")
+        file.write("# Code written by " + user_name + "\n")
+        file.write("# Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n")
+        file.write("# Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n")
+        description = raw_input("Would you like to provide a description of your project? Enter No if a description is not needed \n> ")
+
+        if not(description == "No" or description == "NO" or description == "no"):
+            file.write("# " + description)
+
+    elif extension[0] == "html":
+        file.write("<!-- Code written by " + user_name + " -->\n")
+        file.write("<!-- Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + " -->\n")
+        file.write("<!-- Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + " -->\n")
+        description = raw_input("Would you like to provide a description of your project? Enter No if a description is not needed \n> ")
+
+        if not(description == "No" or description == "NO" or description == "no"):
+            file.write("<!-- " + description + " -->")
+
+
+    elif extension[0] == "css":
+        file.write("/* Code written by " + user_name + " */\n")
+        file.write("/* Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + " */\n")
+        file.write("/* Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + " */\n")
+        description = raw_input("Would you like to provide a description of your project? Enter No if a description is not needed \n> ")
+
+        if not(description == "No" or description == "NO" or description == "no"):
+            file.write("/* " + description + " */")
+
+    file.close()
 
 except:
         print "Not enough arguments"
-

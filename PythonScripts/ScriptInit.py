0a14a17f4f091442fdfbaefb375c3988b7355efb
robgoyal <robin_goyal@hotmail.com>
2016-07-20 22:52:43 -0400

    Added C functionality and argv error check

--- PythonScripts/ScriptInit.py
+++ PythonScripts/ScriptInit.py
@@ -15,24 +15,34 @@ from sys import argv
 import subprocess
 import datetime
 
-script, filename, user_name = argv
-current_time = datetime.datetime.now()
+try:
+    script, filename, user_name = argv
+    current_time = datetime.datetime.now()
 
-extension = filename.split(".")[1:]
+    extension = filename.split(".")[1:]
 
-if extension[0] == "py":
-	subprocess.call(["touch", filename])
+    if extension[0] == "py":
+        subprocess.call(["touch", filename])
 
-	file = open(filename, 'w')
-	file.write("#!/usr/bin/env/python\n\n")
-	file.write("# Code written by " + user_name + "\n")
-	file.write("# Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n")
-	file.write("# Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n\n")
-	description = raw_input("Would you like to provide a description of your project? Press RETURN if no description is needed. \n> ")
-	file.write("# " + description)
+        file = open(filename, 'w')
+        file.write("#!/usr/bin/env/python\n\n")
+        file.write("# Code written by " + user_name + "\n")
+        file.write("# Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n")
+        file.write("# Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "\n\n")
+        description = raw_input("Would you like to provide a description of your project? Press RETURN if no description is needed. \n> ")
+        file.write("# " + description)
+        file.close()
 
-file.close()
+    elif extension[0] == "c":
+        subprocess.call(["touch", filename])
 
+        file = open(filename, 'w')
+        file.write("/* Code written by " + user_name + "*/\n")
+        file.write("/* Created on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "*/\n")
+        file.write("/* Last updated on " + current_time.strftime("%d-%m-%Y %H:%M").split(' ')[0] + "*/\n\n")
+        description = raw_input("Would you like to provide a description of our project? Press RETURN if no description is needed. \n> ")
+        file.write("/* " + description + " */")
 
-
+except:
+        print "Not enough arguments"
 
